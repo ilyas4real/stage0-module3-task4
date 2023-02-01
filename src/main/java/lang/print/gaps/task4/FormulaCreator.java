4bf85bfa4ef061c38d92326f163ffcd214edf74e
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-02-01 23:21:34 +0100

    completed assginments

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte a = 2;
+        byte b = 3;
+        double res = (9*(a*a) - 5*b + 2 + a - 7)*((a+b-4*a*b)/2);
+        System.out.println(res);
     }
 }
